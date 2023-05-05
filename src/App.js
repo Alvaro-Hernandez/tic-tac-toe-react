40e5197b2c80fbf569b9aab5fd0126989e2134c8
Alvaro-Hernandez <alvaropineda606@gmail.com>
2023-05-05 02:13:36 -0600

    Board component update v2

--- src/App.js
+++ src/App.js
@@ -20,4 +20,34 @@ function Board({xIsNext, squares, onPlay}) {
     }
     onPlay(nextSquares);
   }
+  const winner = calculateWinner(squares);
+
+  let status;
+
+  if (winner) {
+    status = "Ganador: " + winner;
+  } else {
+    status = "Proximo Jugador: " + (xIsNext ? "X" : "0");
+  }
+
+  return (
+    <>
+      <div className="status">{status}</div>
+      <div className="board-row">
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+      </div>
+    </>
+  );
 }
