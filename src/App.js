fe0a18a5c568d8633ff289d904d5e5f2b6374634
Alvaro-Hernandez <alvaropineda606@gmail.com>
2023-05-06 14:42:39 -0600

    Updating the function handlePlay v2 and the top  component v2

--- src/App.js
+++ src/App.js
@@ -56,10 +56,12 @@ export default function Game(){
   const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const [currentMove, setCurrentMove] = useState(0);
-  const currentSquares = history[history.length -1];
+  const currentSquares = history[currentMove];
 
   function handlePlay(nextSquares) {
-    setHistory([...history, nextSquares]);
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length -1 );
     setXIsNext(!xIsNext);
   }
 
