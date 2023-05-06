a9200b17e9b246966f51a7f679950c4e95ac2ad7
Alvaro-Hernandez <alvaropineda606@gmail.com>
2023-05-05 22:07:22 -0600

    Creating the handlePlay component to update the game v1

--- src/App.js
+++ src/App.js
@@ -53,5 +53,12 @@ function Board({xIsNext, squares, onPlay}) {
 }
 
 export default function Game(){
-  
+  const [xIsNext, setXIsNext] = useState(true);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const currentSquares = history[history.length -1];
+
+  function handlePlay(nextSquares) {
+    setHistory([...history, nextSquares]);
+    setXIsNext(!xIsNext);
+  }
 }
