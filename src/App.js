1c19ffabb8bfa8d1e6fb70826e957b14b692172a
Alvaro-Hernandez <alvaropineda606@gmail.com>
2023-05-06 14:32:48 -0600

    Updating the jump to function - adding functionality v2

--- src/App.js
+++ src/App.js
@@ -55,6 +55,7 @@ function Board({xIsNext, squares, onPlay}) {
 export default function Game(){
   const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
   const currentSquares = history[history.length -1];
 
   function handlePlay(nextSquares) {
@@ -63,7 +64,8 @@ export default function Game(){
   }
 
   function jumpTo(nextMove) {
-
+    setCurrentMove(nextMove);
+    setXIsNext(nextMove % 2 === 0);
   }
 
   const moves = history.map((squares, move) => {
