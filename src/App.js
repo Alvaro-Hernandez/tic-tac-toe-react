1436ff004a0a8cb9d150eb6f08599c443d52070c
Alvaro-Hernandez <alvaropineda606@gmail.com>
2023-05-05 23:20:56 -0600

    Creating a function to calculate the winner

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function Square({value, onSquareClick}) {
 
 function Board({xIsNext, squares, onPlay}) {
   function handleClick(i) {
-    if (squares[i] || calculateWiner(squares)) {
+    if (squares[i] || calculateWinner(squares)) {
       return;
     }
     const nextSquares = squares.slice();
@@ -72,4 +72,25 @@ export default function Game(){
       </div>
     </div>
   )
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6],
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] ===squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
